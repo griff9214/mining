a4b359b43a9a0a6550a302a8bab4bd803d56193a
griff9214 <griff@list.ru>
2018-04-03 13:32:03 +0300

    адаптив пошел

--- app/app_js/common.js
+++ app/app_js/common.js
@@ -1,5 +1,33 @@
-function a () {
-    console.log('asdas');
-}
-
-a();
+$('form button').click(function (e) {
+    console.log(1);
+    e.preventDefault();
+});
+$('div.modal-dialog').onmousedown(function (e) {
+    e.preventDefault();
+});
+// $('form button').click(function (event) {
+//     var valid = true;
+//
+//     if (valid) {
+//         formData = $(this).closest('form').serialize();
+//         $.ajax({
+//             type: 'POST',
+//             url: '/mailer.php',
+//             context: this,
+//             data: formData,
+//             beforeSend: function () {
+//                 $(this).closest(".modal").modal('hide');
+//                 $("#ResultModal").remove();
+//             },
+//             success: function (data) {
+//                 $("body").append(data);
+//                 $("#ResultModal").modal();
+//             },
+//             error: function (xhr, str) {
+//                 alert('Ð’Ð¾Ð·Ð½Ð¸ÐºÐ»Ð° Ð¾ÑˆÐ¸Ð±ÐºÐ°: ' + xhr.responseCode);
+//             }
+//         });
+//     }
+//
+//     event.preventDefault();
+// });
